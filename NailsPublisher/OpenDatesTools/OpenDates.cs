58c0cb168380c0b09706dac32e200d4abd9163c6
lvd536 <nbikiti@gmail.com>
2025-02-23 23:13:31 +0400

    Create OpenDates.cs

---
+++ NailsPublisher/OpenDatesTools/OpenDates.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NailsPublisher.Database;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+ 
+namespace NailsPublisher.OpenDatesTools;
+
+public class OpenDates
+{
+    public async Task CreateOpenDatesAsync(ITelegramBotClient botClient, Message msg)
+    {
+        using (ApplicationContext db = new ApplicationContext())
+        {
+            var chat = db.Chats
+                .Include(u => u.Users)
+                .ThenInclude(u => u.Posts)
+                .FirstOrDefault(u => u.ChatId == msg.Chat.Id);
+            var user = chat?.Users.FirstOrDefault(u => u.UserId == msg.From?.Id);
+        }
+    }
+}
