7765a35a1ffc67f872af45c21c0c37fb41fbdec6
lvd536 <nbikiti@gmail.com>
2025-02-25 17:30:07 +0400

    Add rewrite cmd logic, add DbNull handlers to OpenDates.cs

--- NailsPublisher/OpenDatesTools/OpenDates.cs
+++ NailsPublisher/OpenDatesTools/OpenDates.cs
@@ -17,6 +17,15 @@ public static class OpenDates
                 .ThenInclude(u => u.OpenDates)
                 .FirstOrDefault(u => u.ChatId == msg.Chat.Id);
             var user = chat?.Users.FirstOrDefault(u => u.UserId == msg.From?.Id);
+            if (chat is null || user is null)
+            {
+                await DbMethods.InitializeDbAsync(msg);
+                chat = db.Chats
+                    .Include(u => u.Users)
+                    .ThenInclude(u => u.OpenDates)
+                    .FirstOrDefault(u => u.ChatId == msg.Chat.Id);
+                user = chat?.Users.FirstOrDefault(u => u.UserId == msg.From?.Id);
+            }
             var openDate = new EntityList.OpenDate
             {
                 Date = DateTime.Parse(date),
@@ -35,6 +44,15 @@ public static class OpenDates
                 .ThenInclude(u => u.OpenDates)
                 .FirstOrDefault(u => u.ChatId == msg.Chat.Id);
             var user = chat?.Users.FirstOrDefault(u => u.UserId == msg.From?.Id);
+            if (chat is null || user is null)
+            {
+                await DbMethods.InitializeDbAsync(msg);
+                chat = db.Chats
+                    .Include(u => u.Users)
+                    .ThenInclude(u => u.OpenDates)
+                    .FirstOrDefault(u => u.ChatId == msg.Chat.Id);
+                user = chat?.Users.FirstOrDefault(u => u.UserId == msg.From?.Id);
+            }
             var channel = user.ChannelId != 0 ? user.ChannelId : msg.Chat.Id;
             var message = "Календарь записей:\n";
             if (user.OpenDates.Count < 0)
@@ -67,6 +85,31 @@ public static class OpenDates
                 chat.LastDateMessageId = sendMessage.MessageId;
                 await db.SaveChangesAsync();
                 await botClient.SendMessage(msg.From.Id,"Календарь свободных и занятых дат был успешно отправлен. Впредь изменения будут появляться в этом сообщении. Если вы хотите пересоздать календарь - просто удалите сообщение с календарем", ParseMode.Html);
+            }
+        }
+    }
+        public static async Task RewriteOpenDatesAsync(ITelegramBotClient botClient, Message msg)
+    {
+        using (ApplicationContext db = new ApplicationContext())
+        {
+            var chat = db.Chats
+                .Include(u => u.Users)
+                .ThenInclude(u => u.OpenDates)
+                .FirstOrDefault(u => u.ChatId == msg.Chat.Id);
+            if (chat is null)
+            {
+                await DbMethods.InitializeDbAsync(msg);
+                chat = db.Chats
+                    .Include(u => u.Users)
+                    .ThenInclude(u => u.OpenDates)
+                    .FirstOrDefault(u => u.ChatId == msg.Chat.Id);
+            }
+            if (chat.LastDateMessageId == 0) await botClient.SendMessage(msg.From.Id,"У вас не установлено отслеживаемое сообщение. Чтобы установить его необходимо 1 раз отправить сообщение в канал: /set в канале, /send после этого", ParseMode.Html);
+            else
+            {
+                chat.LastDateMessageId = 0;
+                await db.SaveChangesAsync();
+                await botClient.SendMessage(msg.From.Id,"Успешно обнулил отслеживаемое сообщение!", ParseMode.Html);
             }
         }
     }
