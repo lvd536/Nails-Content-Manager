2d01369ca8982252206a81920e3945379c15a04d
lvd536 <nbikiti@gmail.com>
2025-03-03 18:52:24 +0400

    Change step construction to switch

--- NailsPublisher/Tools/PostTools/PostLoop.cs
+++ NailsPublisher/Tools/PostTools/PostLoop.cs
@@ -3,11 +3,12 @@ using NailsPublisher.Database;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+
 namespace NailsPublisher.PostTools;
 
 public static class PostLoop
 {
-        public static async Task PostLoopAsync(ITelegramBotClient botClient, Message msg)
+    public static async Task PostLoopAsync(ITelegramBotClient botClient, Message msg)
     {
         using (ApplicationContext db = new ApplicationContext())
         {
@@ -17,90 +18,102 @@ public static class PostLoop
 
             if (post?.Step == "Finally") return;
 
-            if (post?.Step == "Start")
+            switch (post?.Step)
             {
-                await botClient.SendMessage(msg.Chat.Id,
-                    $"Установите описание для поста: ", ParseMode.Html);
-                post.Step = "Description";
-                await db.SaveChangesAsync();
-            }
-            else if (post?.Step == "Description")
-            {
-                if (msg.Text == "-")
+                case "Start":
                 {
-                    post.Description = msg.Text;
                     await botClient.SendMessage(msg.Chat.Id,
-                        $"Пост не будет содержать описание. Напишите Цену для поста: ", ParseMode.Html);
-                    post.Step = "Price";
+                        $"Установите описание для поста: ", ParseMode.Html);
+                    post.Step = "Description";
                     await db.SaveChangesAsync();
+                    break;
                 }
-                else
+                case "Description":
                 {
-                    post.Description = msg.Text;
-                    await botClient.SendMessage(msg.Chat.Id,
-                        $"Вы установили описание поста на {msg.Text}. Напишите Цену для поста: ", ParseMode.Html);
-                    post.Step = "Price";
-                    await db.SaveChangesAsync();
-                }
-            }
-            else if (post?.Step == "Price")
-            {
-                try
-                {
-                    post.Price = short.Parse(msg.Text);
-                }
-                catch (FormatException)
-                {
-                    await botClient.SendMessage(msg.Chat.Id, "Для цены необходимо указать <b>только цифры</b>!",
-                        ParseMode.Html);
-                    return;
-                }
-
-                await botClient.SendMessage(msg.Chat.Id,
-                    $"Вы установили цену поста на {msg.Text}. Отправьте Фото или Видео для поста: ", ParseMode.Html);
-                post.Step = "Photo";
-                await db.SaveChangesAsync();
-            }
-            else if (post?.Step == "Photo")
-            {
-                var channel = user.ChannelId != 0 ? user.ChannelId : msg.Chat.Id;
-                var message = String.Empty;
-                if (post.Description == "-")
-                {
-                    message = $"<blockquote><b>Цена:</b> <code>{post.Price}Р</code></blockquote>";
-                }
-                else
-                {
-                    message =
-                        $"<blockquote><b>Описание:</b> <code>{post.Description}</code></blockquote>\n" +
-                        $"<blockquote><b>Цена:</b> <code>{post.Price}Р</code></blockquote>";
-                }
-                try
-                {
-                    if (msg.Type == MessageType.Photo)
+                    if (msg.Text == "-")
                     {
-                        await botClient.SendPhoto(channel, msg.Photo.Last(), message, ParseMode.Html);
-                    }
-                    else if (msg.Type == MessageType.Video)
-                    {
-                        await botClient.SendVideo(channel, msg.Video, message, ParseMode.Html);
+                        post.Description = msg.Text;
+                        await botClient.SendMessage(msg.Chat.Id,
+                            $"Пост не будет содержать описание. Напишите Цену для поста: ", ParseMode.Html);
+                        post.Step = "Price";
+                        await db.SaveChangesAsync();
                     }
                     else
                     {
-                        await botClient.SendMessage(msg.From.Id, "Вы можете указать в этом пункте <b>только</b> фото или видео!", ParseMode.Html);
+                        post.Description = msg.Text;
+                        await botClient.SendMessage(msg.Chat.Id,
+                            $"Вы установили описание поста на {msg.Text}. Напишите Цену для поста: ", ParseMode.Html);
+                        post.Step = "Price";
+                        await db.SaveChangesAsync();
+                    }
+
+                    break;
+                }
+                case "Price":
+                {
+                    try
+                    {
+                        post.Price = short.Parse(msg.Text);
+                    }
+                    catch (FormatException)
+                    {
+                        await botClient.SendMessage(msg.Chat.Id, "Для цены необходимо указать <b>только цифры</b>!",
+                            ParseMode.Html);
                         return;
                     }
-                    await botClient.SendMessage(msg.From.Id, "Успешно отправил пост!", ParseMode.Html);
-                }
-                catch (ArgumentNullException)
-                {
-                    await botClient.SendMessage(msg.From.Id,
-                        "Вам необходимо отправить фото. Другие виды медиа не принимаются", ParseMode.Html);
-                    return;
-                }
 
-                post.Step = "Finally";
-                await db.SaveChangesAsync();
+                    await botClient.SendMessage(msg.Chat.Id,
+                        $"Вы установили цену поста на {msg.Text}. Отправьте Фото или Видео для поста: ",
+                        ParseMode.Html);
+                    post.Step = "Photo";
+                    await db.SaveChangesAsync();
+                    break;
+                }
+                case "Photo":
+                {
+                    var channel = user.ChannelId != 0 ? user.ChannelId : msg.Chat.Id;
+                    var message = String.Empty;
+                    if (post.Description == "-")
+                    {
+                        message = $"<blockquote><b>Цена:</b> <code>{post.Price}Р</code></blockquote>";
+                    }
+                    else
+                    {
+                        message =
+                            $"<blockquote><b>Описание:</b> <code>{post.Description}</code></blockquote>\n" +
+                            $"<blockquote><b>Цена:</b> <code>{post.Price}Р</code></blockquote>";
+                    }
+
+                    try
+                    {
+                        if (msg.Type == MessageType.Photo)
+                        {
+                            await botClient.SendPhoto(channel, msg.Photo.Last(), message, ParseMode.Html);
+                        }
+                        else if (msg.Type == MessageType.Video)
+                        {
+                            await botClient.SendVideo(channel, msg.Video, message, ParseMode.Html);
+                        }
+                        else
+                        {
+                            await botClient.SendMessage(msg.From.Id,
+                                "Вы можете указать в этом пункте <b>только</b> фото или видео!", ParseMode.Html);
+                            return;
+                        }
+
+                        await botClient.SendMessage(msg.From.Id, "Успешно отправил пост!", ParseMode.Html);
+                    }
+                    catch (ArgumentNullException)
+                    {
+                        await botClient.SendMessage(msg.From.Id,
+                            "Вам необходимо отправить фото. Другие виды медиа не принимаются", ParseMode.Html);
+                        return;
+                    }
+
+                    post.Step = "Finally";
+                    await db.SaveChangesAsync();
+                    break;
+                }
             }
         }
     }
