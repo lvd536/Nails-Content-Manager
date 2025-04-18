f491aacf72c52aaee3d884b1e9575428a8ba89c8
lvd536 <nbikiti@gmail.com>
2025-04-18 22:12:30 +0400

    Add all new metric methods template

---
+++ NailsPublisher/Tools/Utils/MetricsCommand.cs
@@ -0,0 +1,53 @@
+﻿using NailsPublisher.Database;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace NailsPublisher.Tools.Metrics;
+
+public static class MetricsCommand
+{
+    private static async Task ProfitAsync(ITelegramBotClient botClient, Message msg,  int daysPeriod) // статистика прибыли
+    {
+        using (ApplicationContext db = new ApplicationContext())
+        {
+            var chat = await DbMethods.GetChatByMessageAsync(db, msg);
+            var user = await DbMethods.GetUserByChatAsync(db, chat, msg);
+        }
+    }
+
+    private static async Task ExpensesAsync(ITelegramBotClient botClient, Message msg,  int daysPeriod) // статистика по расходам
+    {
+        using (ApplicationContext db = new ApplicationContext())
+        {
+            var chat = await DbMethods.GetChatByMessageAsync(db, msg);
+            var user = await DbMethods.GetUserByChatAsync(db, chat, msg);
+        }
+    }
+
+    private static async Task ProductsAsync(ITelegramBotClient botClient, Message msg) // аналитика товаров
+    {
+        using (ApplicationContext db = new ApplicationContext())
+        {
+            var chat = await DbMethods.GetChatByMessageAsync(db, msg);
+            var user = await DbMethods.GetUserByChatAsync(db, chat, msg);
+        }
+    }
+
+    private static async Task OrdersAsync(ITelegramBotClient botClient, Message msg) // статистика по заказам
+    {
+        using (ApplicationContext db = new ApplicationContext())
+        {
+            var chat = await DbMethods.GetChatByMessageAsync(db, msg);
+            var user = await DbMethods.GetUserByChatAsync(db, chat, msg);
+        }
+    }
+    
+    private static async Task SummaryAsync(ITelegramBotClient botClient, Message msg) // общая сводка
+    {
+        using (ApplicationContext db = new ApplicationContext())
+        {
+            var chat = await DbMethods.GetChatByMessageAsync(db, msg);
+            var user = await DbMethods.GetUserByChatAsync(db, chat, msg);
+        }
+    }
+}
